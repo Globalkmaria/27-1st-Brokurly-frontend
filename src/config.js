8898e57ccf5a915b5c2e0e183191ec407b4d4a51
성현Maria 김 <mariapro@mariaPro.local>
2022-02-13 00:12:21 +0900

    Add: popup message after item is added to the cart

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
-const BASE_URL_CHANGMIN = 'http://15.165.18.240:8000';
+const BASE_URL = 'http://3.34.52.65:8000';
 
 const API = {
-  signin: `${BASE_URL_CHANGMIN}/users/signin`,
-  signup: `${BASE_URL_CHANGMIN}/users/signup`,
-  signUsername: `${BASE_URL_CHANGMIN}/users/username`,
-  signEmail: `${BASE_URL_CHANGMIN}/users/email`,
-  product: `${BASE_URL_CHANGMIN}/product`,
-  cart: `${BASE_URL_CHANGMIN}/cart`,
-  orders: `${BASE_URL_CHANGMIN}/orders`,
+  signin: `${BASE_URL}/users/signin`,
+  signup: `${BASE_URL}/users/signup`,
+  signUsername: `${BASE_URL}/users/username`,
+  signEmail: `${BASE_URL}/users/email`,
+  product: `${BASE_URL}/product`,
+  cart: `${BASE_URL}/cart`,
+  orders: `${BASE_URL}/orders`,
 };
 
 export default API;
--- src/pages/ProductList/ProducListContent/Products/Product/Product.js
+++ src/pages/ProductList/ProducListContent/Products/Product/Product.js
@@ -3,9 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import './Product.scss';
 
-function Product({ product, putInfoIntoModal }) {
-  const { name, image, price, introduction, id } = product;
+function Product({ product, putInfoIntoModal, removePopupState }) {
+  const { name, image, price, introduction, id, popup } = product;
   const cartBtn = useRef();
+
+  if (popup) {
+    setTimeout(() => {
+      removePopupState(id);
+    }, 2000);
+  }
 
   const navigate = useNavigate();
 
@@ -18,6 +24,7 @@ function Product({ product, putInfoIntoModal }) {
   return (
     <article className="product" onClick={gotoProductDetail}>
       <div className="imgContainer">
+        {popup && <span className="popup">장바구니에 추가되었습니다</span>}
         <img src={image} alt={name} />
         <button
           className="cartBtn"
